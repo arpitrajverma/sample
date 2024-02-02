457be88bcc76a7921a21a405a0ae5f48547511e8
Arpit Raj <arpitrajverma@gmail.com>
2024-02-02 18:18:13 +0530

    initial commit

---
+++ Trie_Implementation.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+class TrieNode
+{
+public:
+  char data;
+  TrieNode *children[128];
+  bool isTerminal;
+  TrieNode(char ch)
+  {
+    data = ch;
+    for (int i = 0; i < 128; i++)
+    {
+      children[i] = NULL;
+    }
+    isTerminal = false;
+  }
+};
+class Trie
+{
+public:
+  TrieNode *root;
+  Trie()
+  {
+    root = new TrieNode('\0');
+  }
+
+  void insertUtil(TrieNode *root, string word)
+  {
+    // base case
+    if (word.length() == 0)
+    {
+      root->isTerminal = true; // it will be last character
+      return;
+    }
+    int index = word[0]; // assume all char are capitals
+    TrieNode *child;
+    // present
+    if (root->children[index] != NULL)
+    {
+      child = root->children[index];
+    }
+    // absent
+    else
+    {
+      child = new TrieNode(word[0]); // new node create..then insert into the array
+      root->children[index] = child;
+    }
+    insertUtil(child, word.substr(1));
+  }
+
+  void insertWord(string word)
+  {
+    insertUtil(root, word);
+  }
+
+  bool searchUtil(TrieNode *root, string word)
+  {
+    if (word.length() == 0)
+    {
+      return root->isTerminal;
+    }
+    int index = word[0];
+    TrieNode *child;
+    if (root->children[index] != NULL)
+    {
+      child = root->children[index];
+    }
+    else
+    {
+      return false;
+    }
+    return searchUtil(child, word.substr(1));
+  }
+
+  bool searchWord(string word)
+  {
+    return searchUtil(root, word);
+  }
+};
+int main()
+{
+  Trie *t = new Trie();
+
+  t->insertWord("abcd");
+  t->insertWord("do");
+  t->insertWord("time");
+
+  cout
+      << "present or not " << t->searchWord("tim ") << endl;
+
+  return 0;
+}
